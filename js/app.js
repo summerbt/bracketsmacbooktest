4984554e44e6acc0397d1fa6e756a2585c9698a2
Summer Barton-Taylor <summerbartontaylor@gmail.com>
2015-12-30 15:43:35 -0600

    Geolocation Update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,43 @@
 function showResults(arr) {
-    var html = "";
-    $.each(arr, function (i, value) {
-        var title = value.Title;
-        console.log(title);
-        html += '<p>' + title + '</p>';
+    console.log(arr);
+    //    var html = "";
+    //    $.each(arr, function (i, value) {
+    //        var title = value.snippet.title;
+    //        console.log(title);
+    //        html += '<p>' + title + '</p>';
+    //    });
+    //    $('#search-results').html(html);
+}
+
+function getRequest(searchTerm) {
+    alert('getRequest');
+    var params = {
+        size: '600x300',
+        location: '46.414382,10.013988',
+        heading: '151.78',
+        pitch: '-0.76',
+        key: 'AIzaSyCmdvpctynhzt2G5bT6Ybj246HrQwaoi3U'
+    };
+    url = 'https://maps.googleapis.com/maps/api/streetview';
+
+    $.getJSON(url, params, function (data) {
+        alert('getJSON');
+        console.log(data);
+        //        showResults(data.items);
     });
-    $('#search-results').html(html);
 }
 
 
-
 $(document).ready(function () {
+    //    alert('here');
+    //    $.getJSON('https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=AIzaSyCmdvpctynhzt2G5bT6Ybj246HrQwaoi3U', function (data) {
+    //        console.log(data);
+    //
+    //    });
     $('#search-term').submit(function (event) {
         event.preventDefault();
         var searchTerm = $('#query').val();
-        $.getJSON('http://www.omdbapi.com/?s=' + searchTerm + '&r=json', function (data) {
-            console.log(data);
-            var arr = data.Search
-            showResults(arr);
-        }, 'json');
+        $('#query').val('');
+        getRequest(searchTerm);
     });
-
 });
